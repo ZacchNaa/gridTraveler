941d3db786e593d6ec6dee606e815524df660613
ZacchNaa <2016.naaz@gmail.com>
2021-07-10 11:15:51 +0000

    Improve grid function

--- scripts.js
+++ scripts.js
@@ -10,6 +10,19 @@ const gridTraveler = (rows, columns) => {
   return gridTraveler(rows - 1, columns) + gridTraveler(rows, columns - 1);
 };
 
-console.log(gridTraveler(0, 1));
-console.log(gridTraveler(1, 1));
-console.log(gridTraveler(2, 3));
+// improving the function = memoization
+const gridTravelerMemo = (rows, columns, memo = {}) => {
+  // set the args[rows, columns] as key of the memo object
+  const key = `${rows}, ${columns}`;
+  // check if key exist in the memo
+  if (key in memo) return memo[key];
+  if (rows === 1 && columns === 1) return 1;
+  if (rows === 0 || columns === 0) return 0;
+  memo[key] =
+    gridTravelerMemo(rows - 1, columns) + gridTravelerMemo(rows, columns - 1);
+  return memo[key];
+};
+
+console.log(gridTravelerMemo(0, 1));
+console.log(gridTravelerMemo(1, 1));
+console.log(gridTravelerMemo(2, 3));
